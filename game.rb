48bb323342676b42334c5e56cbd4af4c0091cac8
Maria <mliberov@gmail.com>
2015-03-29 14:06:57 -0400

    Fix functionality so that duplicate or invalid guesses do not count in guess count

--- game.rb
+++ game.rb
@@ -7,8 +7,7 @@ class Game
   end
 
   def pick_number
-    @count = @count + 1
-    if @count < 6
+    if @count < 5
       puts "Please select a number from 1 to 100."
       @number = gets.chomp.to_i
       #@previous_guesses = []
@@ -16,8 +15,8 @@ class Game
     else
       puts "You have no guesses left. Your game is over."
       puts "Would you like to play again? (Y/N)"
-      @response = gets.chomp
-      if @response == "Y"
+      @response = gets.chomp.downcase
+      if @response == "y" || @response == "yes"
         Game.new.pick_number
       else
         exit
@@ -39,16 +38,18 @@ class Game
     elsif @number > @chosen && @number < 101
       puts "Your number is too high."
       @previous_guesses << @number
+      @count = @count + 1
       pick_number
       #guesses
     elsif @number < @chosen && @number > 0
       puts "Your number is too low."
       @previous_guesses << @number
+      @count = @count + 1
       pick_number
       #guesses
     else
       puts "You have chosen an invalid number."
-      @previous_guesses << @number
+      #@previous_guesses << @number
       pick_number
       #guesses
     end
